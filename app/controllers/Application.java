512525d6e6ba00ef1d4a41397642192eee6299c1
nivae <gannivae@gmail.com>
2014-04-14 16:44:40 +0800

    Dynamic webpage created

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
+import play.data.validation.Validation;
 import play.mvc.*;
 
 import java.util.*;
@@ -22,5 +24,30 @@ public class Application extends Controller {
     	renderArgs.put("blogTitle",Play.configuration.getProperty("blog.title"));
     	renderArgs.put("blogBaseline",Play.configuration.getProperty("blog.baseline"));
     }
-
+    public static void show(Long id)
+    {
+    	Post post=Post.findById(id);
+    	render(post);
+    }
+	
+//    public Post previous(Date posteddate)
+//    {
+//    	return Post.find("posteddate < ? order by posteddate desc",posteddate).first();
+//    	
+//    }
+//    public Post next(Date posteddate)
+//    {
+//    	return Post.find("posteddate > ? order by posteddate asc",posteddate).first();	
+//    }
+    public static void postcomment(Long postid,@Required String author,@Required String content)
+    {
+    	Post post=Post.findById(postid);
+    	if(validation.hasErrors())
+    	{
+    		render("Application/show.html",post);
+    	}
+    	post.addcomment(author, content);
+    	flash.success("Thanks for posting %s",author);
+    	show(postid);
+    }
 }
